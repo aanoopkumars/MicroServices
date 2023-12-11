797ebf658450d310a9201c8ffda5a795880df2e5
anoopkumars <anoopkumars@localhost>
2023-12-11 22:02:31 +0530

    Adding Internationalisation

--- src/main/java/com/microservices/ucan/restwebservice1/helloworld/HelloWorldController.java
+++ src/main/java/com/microservices/ucan/restwebservice1/helloworld/HelloWorldController.java
@@ -1,15 +1,35 @@
 package com.microservices.ucan.restwebservice1.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	
+	private MessageSource messageSource;
+	
+	
+	
+    public HelloWorldController(MessageSource messageSource) {
+		super();
+		this.messageSource = messageSource;
+	}
+
+	// @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     @GetMapping(path = "/hello-world")
     public String sayHello() {
         return "Hi my dear, pls check yay dude";
+    }
+    
+    @GetMapping(path = "/hello-world-i18n")   // if we pass Accept-Language header with value fr , it selects value from messages_fr.properties.
+    public String sayHelloi18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "default message", locale);
     }
 
     @GetMapping(path="/hello-world-bean")
